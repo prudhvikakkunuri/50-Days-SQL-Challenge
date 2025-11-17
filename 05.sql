3c952859a2fe807c4b653210665f04bf640e236f
Prudhvi <70479269+prudhvikakkunuri@users.noreply.github.com>
2025-11-18 01:13:14 +0530

    Update 05.sql

--- 05.sql
+++ 05.sql
@@ -97,13 +97,3 @@ WHERE drn = 3
 
 
 
-
-
-
-/*	
-Follow me in LinkedIn :: https://www.linkedin.com/in/najirr/
-Follow me in insta :: https://www.instagram.com/zero_analyst/
-Subscribe to our youtube channel :: https://www.youtube.com/@zero_analyst
-*/
-	
-
