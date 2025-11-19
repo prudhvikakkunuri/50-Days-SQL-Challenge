899a78060c480d1f7c72c892736b5ba5b70fba54
Prudhvi <70479269+prudhvikakkunuri@users.noreply.github.com>
2025-11-20 01:56:03 +0530

    Update 04.sql

--- 04.sql
+++ 04.sql
@@ -84,26 +84,3 @@ WHERE pdrn <= 2
 
 
 
-
-
-
-
-
-
-/*	
-Follow me in LinkedIn :: https://www.linkedin.com/in/najirr/
-Follow me in insta :: https://www.instagram.com/zero_analyst/
-Subscribe to our youtube channel :: https://www.youtube.com/@zero_analyst
-*/
-	
-
-
-
-
-
-
-
-
-
-
-
